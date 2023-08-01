e0508cb6551cb9bccaed1ec5f06ea66bd868f60f
Felipe Pereira <felipejiti@hotmail.com>
2023-07-31 21:48:07 -0300

    git add .

---
+++ api/src/main/java/com/faculdade/api/SistemaDeFaculdadeApplication.java
@@ -0,0 +1,13 @@
+package com.faculdade.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeFaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeFaculdadeApplication.class, args);
+	}
+
+}
